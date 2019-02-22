ccc4a9b1225139a770ac781c0baaedfa0d4b838e
panomza <47875654+panomza@users.noreply.github.com>
2019-02-22 14:12:53 +0700

    Revert "Chang Port INPUT"
    This reverts commit 47c80aa319ef0989205e4478a3b3f029f92e6fbb.

--- button/button.ino
+++ button/button.ino
@@ -1,11 +1,11 @@
 
-int Bpow = A0;
+int Bpow = A1;
 int Bp;
 int Lp;
 int stateP = LOW;
 int POW=13;
 
-const int Bspeed = A1;
+const int Bspeed = A0;
 const int M1 =  7;
 const int M2 =  8;
 const int M3 =  9;
@@ -44,7 +44,7 @@ void loop()
     
  if(stateP==0){i=0;}
     
- if ((Bstate != Lstate) && (Lstate == HIGH)&&(stateP==1))
+ if ((Bstate != Lstate) && (Lstate == HIGH))
     {  
     stateS=!stateS;
     i=i+1;
