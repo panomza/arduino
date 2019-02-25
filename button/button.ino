7f1f24fd430cd5e8bf8a30e0f9ef80064bc2f26d
panomza <47875654+panomza@users.noreply.github.com>
2019-02-25 11:28:18 +0700

    Add Plasma
    เพิ่มปุ่มกดพลาสมา และจัดบรรทัดใหม่

--- button/button.ino
+++ button/button.ino
@@ -1,9 +1,17 @@
 
-int Bpow = A0;
+const int Bpow = A0;
+const int POW=13;
 int Bp;
 int Lp;
 int stateP = LOW;
-int POW=13;
+
+
+const int Bplasma = A2;
+const int PLASMA=6;
+int Bpm;
+int Lpm;
+int statePM = LOW;
+
 
 const int Bspeed = A1;
 const int M1 =  7;
@@ -11,14 +19,17 @@ const int M2 =  8;
 const int M3 =  9;
 const int M4 =  10;
 
-int Bstate;
-int Lstate;
+int Bs;
+int Ls;
 int stateS = LOW;
 int i = 0; 
 
 void setup() {
-  pinMode(Bpow, INPUT);
+  pinMode(Bplasma, INPUT);
   pinMode(POW, OUTPUT);
+
+  pinMode(Bpow, INPUT);
+  pinMode(PLASMA, OUTPUT);
   
   pinMode(M1, OUTPUT);
   pinMode(M2, OUTPUT); 
@@ -31,21 +42,29 @@ void setup() {
 void loop()
 {
  // Serial.println(i);
-  Bstate = digitalRead(Bspeed);
-  Bp = digitalRead(Bpow);
+  Bs =  digitalRead(Bspeed);
+  Bp      =  digitalRead(Bpow);
+  Bpm     =  digitalRead(Bplasma);
   
- if ((Bp != Lp) && (Lp == HIGH)){  stateP=!stateP;digitalWrite(POW,stateP); i=1;delay(400);}
-    Lp=!Bp;
-    
+ if ((Bp != Lp) && (Lp == HIGH)){  
+     stateP=!stateP;
+     digitalWrite(POW,stateP); 
+     i=1;delay(400);}
+     Lp=!Bp;   
+  
  if(stateP==0){i=0;}
+
+ if ((Bpm != Lpm) && (Lpm == HIGH)&&(stateP==1)){  
+     statePM=!statePM;
+     digitalWrite(PLASMA,statePM); 
+     delay(300);}
+     Lpm=!Bpm; 
     
- if ((Bstate != Lstate) && (Lstate == HIGH)&&(stateP==1))
-    {  
-    stateS=!stateS;
-    i=i+1;
-    delay(200);
-    }
-    Lstate=!Bstate;
+ if ((Bs != Ls) && (Ls == HIGH)&&(stateP==1)){  
+     stateS=!stateS;
+     i=i+1;
+     delay(300);}
+     Ls=!Bs;
 
   switch (i) {
     case 0:
@@ -53,26 +72,27 @@ void loop()
       digitalWrite(M1,HIGH);  
       digitalWrite(M2,HIGH);
       digitalWrite(M3,HIGH);
+      digitalWrite(PLASMA,HIGH);
       break;
     case 1:
       digitalWrite(M1,LOW);   
-      Serial.println("M1");
+     // Serial.println("M1");
       break;
     case 2:
       digitalWrite(M2,LOW);   digitalWrite(M1,HIGH);
-      Serial.println("M2");
+     // Serial.println("M2");
       break;
     case 3:
       digitalWrite(M3,LOW);   digitalWrite(M2,HIGH);
-      Serial.println("M3");
+     // Serial.println("M3");
       break;
     case 4:
       digitalWrite(M4,LOW);   digitalWrite(M3,HIGH);
-      Serial.println("M4");
+     // Serial.println("M4");
       break;
     case 5:
     digitalWrite(M4,HIGH);
-          i=1;
+    i=1;
       break;
   }
 }
