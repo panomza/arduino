45305bd0dd51906c88fe5434b51470c5851d3124
za <za.kuanten@gmail.com>
2019-03-20 15:37:41 +0700

    add buzzer sound with non conflicting timer

--- button_testbuzzer/button_testbuzzer.ino
+++ button_testbuzzer/button_testbuzzer.ino
@@ -1,3 +1,151 @@
+#include <toneAC.h>
+/////////////////song definition////////////
+/*************************************************
+ * Public Constants
+ *************************************************/
+
+#define NOTE_B0  31
+#define NOTE_C1  33
+#define NOTE_CS1 35
+#define NOTE_D1  37
+#define NOTE_DS1 39
+#define NOTE_E1  41
+#define NOTE_F1  44
+#define NOTE_FS1 46
+#define NOTE_G1  49
+#define NOTE_GS1 52
+#define NOTE_A1  55
+#define NOTE_AS1 58
+#define NOTE_B1  62
+#define NOTE_C2  65
+#define NOTE_CS2 69
+#define NOTE_D2  73
+#define NOTE_DS2 78
+#define NOTE_E2  82
+#define NOTE_F2  87
+#define NOTE_FS2 93
+#define NOTE_G2  98
+#define NOTE_GS2 104
+#define NOTE_A2  110
+#define NOTE_AS2 117
+#define NOTE_B2  123
+#define NOTE_C3  131
+#define NOTE_CS3 139
+#define NOTE_D3  147
+#define NOTE_DS3 156
+#define NOTE_E3  165
+#define NOTE_F3  175
+#define NOTE_FS3 185
+#define NOTE_G3  196
+#define NOTE_GS3 208
+#define NOTE_A3  220
+#define NOTE_AS3 233
+#define NOTE_B3  247
+#define NOTE_C4  262
+#define NOTE_CS4 277
+#define NOTE_D4  294
+#define NOTE_DS4 311
+#define NOTE_E4  330
+#define NOTE_F4  349
+#define NOTE_FS4 370
+#define NOTE_G4  392
+#define NOTE_GS4 415
+#define NOTE_A4  440
+#define NOTE_AS4 466
+#define NOTE_B4  494
+#define NOTE_C5  523
+#define NOTE_CS5 554
+#define NOTE_D5  587
+#define NOTE_DS5 622
+#define NOTE_E5  659
+#define NOTE_F5  698
+#define NOTE_FS5 740
+#define NOTE_G5  784
+#define NOTE_GS5 831
+#define NOTE_A5  880
+#define NOTE_AS5 932
+#define NOTE_B5  988
+#define NOTE_C6  1047
+#define NOTE_CS6 1109
+#define NOTE_D6  1175
+#define NOTE_DS6 1245
+#define NOTE_E6  1319
+#define NOTE_F6  1397
+#define NOTE_FS6 1480
+#define NOTE_G6  1568
+#define NOTE_GS6 1661
+#define NOTE_A6  1760
+#define NOTE_AS6 1865
+#define NOTE_B6  1976
+#define NOTE_C7  2093
+#define NOTE_CS7 2217
+#define NOTE_D7  2349
+#define NOTE_DS7 2489
+#define NOTE_E7  2637
+#define NOTE_F7  2794
+#define NOTE_FS7 2960
+#define NOTE_G7  3136
+#define NOTE_GS7 3322
+#define NOTE_A7  3520
+#define NOTE_AS7 3729
+#define NOTE_B7  3951
+#define NOTE_C8  4186
+#define NOTE_CS8 4435
+#define NOTE_D8  4699
+#define NOTE_DS8 4978
+
+
+//##############**"HE IS A PIRATE" Theme song of Pirates of caribbean**##############//
+int Pirates_note[] = {
+  NOTE_C4, NOTE_C5, NOTE_A3, NOTE_A4,
+  NOTE_AS3, NOTE_AS4, 0
+};
+int change_speed[]={
+  NOTE_F3, NOTE_F4, NOTE_D3, NOTE_D4,
+  NOTE_DS3, NOTE_DS4, 0,
+};
+int turn_off[]={
+  NOTE_CS4, NOTE_DS4,
+  NOTE_DS4, NOTE_GS3,
+  NOTE_G3, NOTE_CS4,
+  NOTE_C4, NOTE_FS4, NOTE_F4, NOTE_E3, NOTE_AS4, NOTE_A4,
+  NOTE_GS4, NOTE_DS4, NOTE_B3,
+  NOTE_AS3, NOTE_A3, NOTE_GS3,
+  0, 0, 0
+};
+
+int Pirates_duration[] = {
+    12, 12, 12, 12,
+  12, 12, 6,
+  3,
+  12, 12, 12, 12,
+  12, 12, 6,
+  3,
+  12, 12, 12, 12,
+  12, 12, 6,
+  3,
+  12, 12, 12, 12,
+  12, 12, 6,
+  6, 18, 18, 18,
+  6, 6,
+  6, 6,
+  6, 6,
+  18, 18, 18, 18, 18, 18,
+  10, 10, 10,
+  10, 10, 10,
+  3, 3, 3
+};
+//###########End of He is a Pirate song#############//
+
+///// play song////
+
+// Melody liberated from the toneMelody Arduino example sketch by Tom Igoe.
+int melody[] = { 262, 196, 196, 220, 196, 0, 247, 262 };
+int noteDurations[] = { 8, 16, 16, 8, 8, 8, 8, 8 };
+
+float midi[127];
+int a = 440; // a is 440 hz...
+
 
 
 float currenttime=0;
@@ -79,9 +227,9 @@ const short int POW=13;         //power output pin
 const short int PLASMA=6;       // plasma button output pin
 const short int M1 =  7;        // motor output pin
 const short int M2 =  8;        // motor output pin
-const short int M3 =  9;        // motor output pin
-const short int M4 =  10;       // motor output pin
-const short int BUZ = 4;        // buzzer output pin
+const short int M3 =  17;        // motor output pin
+const short int M4 =  19;       // motor output pin
+//const short int BUZ = 9;        // buzzer output pin
 const short int AUTO = 11; 
 
 
@@ -159,8 +307,8 @@ Serial.begin(9600);
     Bpow,Bspeed,Bplasma,Btimer,Bauto
     };
 
-  int outputpins[8] = {
-    POW,PLASMA,M1,M2,M3,M4,BUZ,AUTO
+  int outputpins[7] = {
+    POW,PLASMA,M1,M2,M3,M4,AUTO
   };
 
   for(int j=0;j< sizeof(inputpins)/sizeof(1);j++){
@@ -191,6 +339,12 @@ Serial.begin(9600);
   for(int i=0;i<numaverage;i++){
   dust[i]=initialdust;
   }
+  /////////////////set up tone conversion///////////////
+  for (int x = 0; x < 127; ++x)
+{
+   midi[x] = (a / 32) * (2 ^ ((x - 9) / 12));
+}
+
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////////
@@ -469,8 +623,31 @@ void Display(){
 //  Serial.println("display started 2");
 }
 
-
-
+///////////////play sound///////////
+void play_sound(){
+    for (int thisNote = 0; thisNote < sizeof(Pirates_note) / sizeof(int); thisNote++) {
+    int noteDuration = 1000/Pirates_duration[thisNote];
+    toneAC(Pirates_note[thisNote], 10, noteDuration, true); // Play thisNote at full volume for noteDuration in the background.
+    delay(noteDuration * 4 / 3); // Wait while the tone plays in the background, plus another 33% delay between notes.
+  }
+  toneAC();
+}
+void play_sound_change_speed(){
+    for (int thisNote = 0; thisNote < sizeof(change_speed) / sizeof(int); thisNote++) {
+    int noteDuration = 1000/Pirates_duration[thisNote];
+    toneAC(change_speed[thisNote], 10, noteDuration, true); // Play thisNote at full volume for noteDuration in the background.
+    delay(noteDuration * 4 / 3); // Wait while the tone plays in the background, plus another 33% delay between notes.
+  }
+  toneAC();
+}
+void play_sound_turn_off(){
+    for (int thisNote = 0; thisNote < sizeof(turn_off) / sizeof(int); thisNote++) {
+    int noteDuration = 1000/Pirates_duration[thisNote];
+    toneAC(turn_off[thisNote], 10, noteDuration, true); // Play thisNote at full volume for noteDuration in the background.
+    delay(noteDuration * 4 / 3); // Wait while the tone plays in the background, plus another 33% delay between notes.
+  }
+  toneAC();
+}
 ////////////////////////////////////////VIOD////////////////////////////////////////////////
 ////////////////////////////////////////LOOP/////////////////////////////////////////
 
@@ -493,20 +670,17 @@ sensor_dust();
 Display();
 applythespeedswitch();
 
+if(Bp==0 && stateP==1){
 
+  play_sound();
+}
+if(Bs==0){
+  play_sound_change_speed();
+}
 
+if(Bp==0 && stateP==0){
 
-
-
-
-
-
-
-
-
-
-
-
-
+  play_sound_turn_off();
+}
 
 }
