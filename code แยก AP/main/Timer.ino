8ab1fc802f3617e4ba6a45738a6e081d809c91db
za <za.kuanten@gmail.com>
2019-05-10 16:08:29 +0700

    7/05/20119
    เปลี่ยนการแสดงสถานะของ timer จากหลอดไฟ led มาแสดงที่ 7 Segment Display

--- code แยก AP/main/Timer.ino
+++ code แยก AP/main/Timer.ino
@@ -3,7 +3,7 @@ unsigned int timetrig=0;
 unsigned int runtime =0;
 
 void TIMER(){
- runtime=millis();
+ 
     if ((Bt != Lt) && Bt == 0  && (currenttime-timer0 > buttondelay)){
         Lt=Bt;
         Settime++;
@@ -18,45 +18,33 @@ void TIMER(){
       Lt=Bt;      
     }
 ///////////////////////////////////////////////////////////////////////////////.
+  if(timedown==0){Settime=0;}  
+  if(timedown==1){Bp=0;}
   
 if(Settime==0) {timedown=0;} 
 if(Settime>0){  
+  runtime=millis();
         if(runtime - timetrig >1000){
                  timetrig = runtime;
                  timedown=timedown-+1;  
                  Serial.println(timedown);     
          }    
-         
- 
-  if(timedown==0){Settime=0;}  
-  if(timedown==1){Bp=0;}else{Bp=1;}
-//  if(timedown==10){clearstatetime();digitalWrite(T1,1);}
-//  if(timedown==20){clearstatetime();digitalWrite(T2,1);}
-//  if(timedown==30){clearstatetime();digitalWrite(T3,1);}
-//  if(timedown==40){clearstatetime();digitalWrite(T4,1);}
   }
 }
 
 void selecttime(){
   switch(Settime){
     case 1:
-    timedown=11;
+    timedown=10;
     break;
     case 2:
     timedown=20;
     break;
     case 3:
-    timedown=30;
+    timedown=40;
     break;
     case 4:
-    timedown=40;
+    timedown=80;
     break;
   }
 }
-
-//void clearstatetime(){
-//  digitalWrite(T1,0);
-//  digitalWrite(T2,0);
-//  digitalWrite(T3,0);
-//  digitalWrite(T4,0);
-//}
